119c46b4e45411a908fba8d99bd4dcab03e901a9
Sai Yogesh <saiyoges9911@gmail.com>
2025-04-06 10:09:44 -0500

    change1

--- src/main/java/com/devops/DevopsIntegrationApplication.java
+++ src/main/java/com/devops/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "Welcome to Sample Devops App";
+		return "Welcome to Sample Application deployed in Kubernetes using Jenkins Pipeline";
 	}
 
 	public static void main(String[] args) {
